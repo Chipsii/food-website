9d4419592147ccf7a0a72cee91e5525f82437729
Hasib <hasibfire24@gmail.com>
2023-11-02 21:57:35 +0600

    pizza website

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Pizza Website</title>
+        <meta name="description" content="Pizza Shop" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    </div>
+  );
+}
